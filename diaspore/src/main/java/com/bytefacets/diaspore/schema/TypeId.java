675e52cefc6b82c39470d10e4b539da6861a33a4
paulfeuer <code@bytefacets.com>
2025-03-14 21:19:47 -0400

    more utils for TypeId

--- diaspore/src/main/java/com/bytefacets/diaspore/schema/TypeId.java
+++ diaspore/src/main/java/com/bytefacets/diaspore/schema/TypeId.java
@@ -2,9 +2,44 @@
 // SPDX-License-Identifier: MIT
 package com.bytefacets.diaspore.schema;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public final class TypeId {
+    private static final Map<Class<?>, Byte> mapping = new HashMap<>(32);
+
+    static {
+        mapping.put(boolean.class, TypeId.Bool);
+        mapping.put(Boolean.class, TypeId.Bool);
+
+        mapping.put(byte.class, TypeId.Byte);
+        mapping.put(Byte.class, TypeId.Byte);
+
+        mapping.put(short.class, TypeId.Short);
+        mapping.put(Short.class, TypeId.Short);
+
+        mapping.put(char.class, TypeId.Char);
+        mapping.put(Character.class, TypeId.Char);
+
+        mapping.put(int.class, TypeId.Int);
+        mapping.put(Integer.class, TypeId.Int);
+
+        mapping.put(long.class, TypeId.Long);
+        mapping.put(Long.class, TypeId.Long);
+
+        mapping.put(float.class, TypeId.Float);
+        mapping.put(Float.class, TypeId.Float);
+
+        mapping.put(double.class, TypeId.Double);
+        mapping.put(Double.class, TypeId.Double);
+
+        mapping.put(String.class, TypeId.String);
+        mapping.put(Object.class, TypeId.Generic);
+    }
+
     private TypeId() {}
 
+    public static final int Min = 1;
     public static final byte Bool = 1;
     public static final byte Byte = 2;
     public static final byte Short = 3;
@@ -16,6 +51,10 @@ public final class TypeId {
     public static final byte String = 9;
     public static final byte Generic = 10;
     public static final int Max = 10;
+
+    public static byte toId(final Class<?> type) {
+        return mapping.getOrDefault(type, TypeId.Generic);
+    }
 
     public static Class<?> toClass(final byte id) {
         return switch (id) {
@@ -43,7 +82,7 @@ public final class TypeId {
             case Float -> "Float";
             case Double -> "Double";
             case String -> "String";
-            default -> "Object";
+            default -> "Generic";
         };
     }
 }
