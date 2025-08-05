7948b3ec53e7a5687c2332811aa44296495f21c2
paulfeuer <code@bytefacets.com>
2025-08-05 18:13:20 -0400

    jwt client creds and server-side auth

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 tasks.withType<PublishToMavenRepository>().configureEach { enabled = false }
 tasks.withType<PublishToMavenLocal>().configureEach { enabled = false }
 
+val auth0: String by extra
 val grpcVersion = "1.70.0"
 val slfApiVersion: String by extra
 val logbackVersion: String by extra
@@ -21,6 +22,7 @@ dependencies {
 
     implementation("io.grpc:grpc-protobuf:${grpcVersion}")
     implementation("io.grpc:grpc-stub:${grpcVersion}")
-    implementation("io.grpc:grpc-netty-shaded:${grpcVersion}")
+    implementation("io.grpc:grpc-netty:${grpcVersion}")
     implementation("io.netty:netty-transport:${nettyVersion}")
+    implementation("com.auth0:java-jwt:${auth0}")
 }
