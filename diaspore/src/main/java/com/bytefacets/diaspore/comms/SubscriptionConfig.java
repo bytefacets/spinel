b667d75574f7dd8eb9efa7b055567ffa5e17b6ba
paulfeuer <code@bytefacets.com>
2025-08-19 07:45:12 -0400

    adding missing licensing headers

--- diaspore/src/main/java/com/bytefacets/diaspore/comms/SubscriptionConfig.java
+++ diaspore/src/main/java/com/bytefacets/diaspore/comms/SubscriptionConfig.java
@@ -1,3 +1,5 @@
+// SPDX-FileCopyrightText: Copyright (c) 2025 Byte Facets
+// SPDX-License-Identifier: MIT
 package com.bytefacets.diaspore.comms;
 
 import static java.util.Objects.requireNonNull;
--- diaspore/src/test/java/com/bytefacets/diaspore/transform/TransformContinuationTest.java
+++ diaspore/src/test/java/com/bytefacets/diaspore/transform/TransformContinuationTest.java
@@ -1,3 +1,5 @@
+// SPDX-FileCopyrightText: Copyright (c) 2025 Byte Facets
+// SPDX-License-Identifier: MIT
 package com.bytefacets.diaspore.transform;
 
 import static org.mockito.ArgumentMatchers.any;
