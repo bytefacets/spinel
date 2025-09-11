1ef437e5553fd8ad63b5dfb27a0d08f057b27133
Byte Facets <168381862+bytefacets@users.noreply.github.com>
2025-09-10 21:14:55 -0400

    template plugin from gradl (#55)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,16 +4,6 @@ pluginManagement {
     repositories {
         mavenLocal()
         gradlePluginPortal()
-        repositories {
-            maven {
-                name = "GitHubPackages"
-                url = uri("https://maven.pkg.github.com/bytefacets/type-template-processor")
-                credentials {
-                    username = System.getenv("GITHUB_ACTOR") ?: providers.gradleProperty("gpr.user").orElse("no-username").get()
-                    password = System.getenv("GITHUB_TOKEN") ?: providers.gradleProperty("gpr.key").orElse("no-token").get()
-                }
-            }
-        }
     }
 }
 
