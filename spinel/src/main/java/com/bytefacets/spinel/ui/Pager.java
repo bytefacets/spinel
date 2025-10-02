4559ef8692f7e1c4ad016a8944191701a71d2317
paulfeuer <code@bytefacets.com>
2025-10-02 13:30:29 -0400

    pager utility

---
+++ spinel/src/main/java/com/bytefacets/spinel/ui/Pager.java
@@ -0,0 +1,109 @@
+// SPDX-FileCopyrightText: Copyright (c) 2025 Byte Facets
+// SPDX-License-Identifier: MIT
+package com.bytefacets.spinel.ui;
+
+import com.bytefacets.collections.arrays.IntArray;
+import com.bytefacets.collections.functional.IntConsumer;
+import com.bytefacets.collections.functional.IntIterable;
+import com.bytefacets.collections.store.IntChunkStore;
+import com.bytefacets.collections.store.IntStore;
+import java.util.Arrays;
+
+/**
+ * A Paging utility that keeps a compact, bidirectional mapping of row to view port position.
+ */
+public final class Pager {
+    private final IntStore activeRows;
+    private final IntStore rowToPosition;
+    private int[] removedPositions = new int[16];
+    private int numRemoved;
+    private int limit;
+
+    public static Pager pager(final int initialSize) {
+        return pager(initialSize, 128);
+    }
+
+    public static Pager pager(final int initialSize, final int chunkSize) {
+        return new Pager(initialSize, chunkSize);
+    }
+
+    private Pager(final int initialSize, final int chunkSize) {
+        activeRows = new IntChunkStore(initialSize, chunkSize);
+        rowToPosition = new IntChunkStore(initialSize, chunkSize);
+    }
+
+    public void add(final IntIterable rows) {
+        rows.forEach(this::internalAdd);
+    }
+
+    public void remove(final IntIterable rows) {
+        rows.forEach(this::internalRemove);
+        compact();
+    }
+
+    public void rowsInRange(final int offset, final int limit, final IntConsumer consumer) {
+        final int end = Math.min(this.limit, offset + limit);
+        final int start = Math.min(offset, this.limit);
+        for (int position = start; position < end; position++) {
+            consumer.accept(activeRows.getInt(position));
+        }
+    }
+
+    public int size() {
+        return limit;
+    }
+
+    private void internalAdd(final int row) {
+        rowToPosition.setInt(row, limit);
+        activeRows.setInt(limit, row);
+        limit++;
+    }
+
+    private void internalRemove(final int row) {
+        removedPositions = IntArray.ensureEntry(removedPositions, numRemoved);
+        final int position = rowToPosition.getInt(row);
+        removedPositions[numRemoved++] = position;
+    }
+
+    private void compact() {
+        if (numRemoved == 0) {
+            return;
+        }
+
+        Arrays.sort(removedPositions, 0, numRemoved);
+
+        int writePos = removedPositions[0]; // first hole
+        int readPos = removedPositions[0] + 1;
+
+        // index of current removed slot we’re skipping
+        int removeIdx = 1;
+
+        while (readPos < limit) {
+            if (removeIdx < numRemoved && readPos == removedPositions[removeIdx]) {
+                // skip over this removed position
+                readPos++;
+                removeIdx++;
+            } else {
+                // move row down
+                final int row = activeRows.getInt(readPos++);
+                activeRows.setInt(writePos, row);
+                rowToPosition.setInt(row, writePos);
+                writePos++;
+            }
+        }
+
+        // adjust limit
+        limit = writePos;
+        numRemoved = 0;
+    }
+
+    // VisibleForTesting
+    int position(final int row) {
+        return rowToPosition.getInt(row);
+    }
+
+    // VisibleForTesting
+    int row(final int position) {
+        return activeRows.getInt(position);
+    }
+}
---
+++ spinel/src/test/java/com/bytefacets/spinel/ui/PagerTest.java
@@ -0,0 +1,158 @@
+// SPDX-FileCopyrightText: Copyright (c) 2025 Byte Facets
+// SPDX-License-Identifier: MIT
+package com.bytefacets.spinel.ui;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import com.bytefacets.collections.functional.IntConsumer;
+import com.bytefacets.collections.functional.IntIterable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class PagerTest {
+    private final Pager pager = Pager.pager(3);
+    private final Random rnd = new Random(38768768);
+
+    @Test
+    void shouldGrow() {
+        final var rows = rowsInRandomOrder(0, 500);
+        pager.add(iterable(rows));
+        assertThat(pager.size(), equalTo(rows.length));
+        pager.remove(iterable(IntStream.range(100, 200).toArray()));
+        assertThat(pager.size(), equalTo(rows.length - 100));
+    }
+
+    @Nested
+    class AddTests {
+        @Test
+        void shouldAddRows() {
+            pager.add(iterable(5, 6, 7));
+            assertRowOrder(5, 6, 7);
+        }
+
+        @Test
+        void shouldAddAfterRemove() {
+            pager.add(iterable(5, 6, 7, 15, 20, 10));
+            pager.remove(iterable(10, 7, 5));
+            pager.add(iterable(30, 60, 40, 50));
+            assertRowOrder(6, 15, 20, 30, 60, 40, 50);
+            assertThat(pager.size(), equalTo(7));
+        }
+    }
+
+    @Nested
+    class RemoveTests {
+        @BeforeEach
+        void setUp() {
+            pager.add(iterable(5, 6, 7, 15, 20, 10));
+        }
+
+        @Test
+        void shouldRemoveFirstRow() {
+            pager.remove(iterable(5));
+            assertRowOrder(6, 7, 15, 20, 10);
+            assertThat(pager.size(), equalTo(5));
+        }
+
+        @Test
+        void shouldRemoveLastRow() {
+            pager.remove(iterable(10));
+            assertRowOrder(5, 6, 7, 15, 20);
+            assertThat(pager.size(), equalTo(5));
+        }
+
+        @Test
+        void shouldRemoveMiddle() {
+            pager.remove(iterable(7));
+            assertRowOrder(5, 6, 15, 20, 10);
+            assertThat(pager.size(), equalTo(5));
+        }
+
+        @Test
+        void shouldRemoveMultipleMiddle() {
+            pager.remove(iterable(20, 7));
+            assertRowOrder(5, 6, 15, 10);
+            assertThat(pager.size(), equalTo(4));
+        }
+
+        @Test
+        void shouldRemoveMultipleUnordered() {
+            pager.remove(iterable(20, 6, 7, 15));
+            assertRowOrder(5, 10);
+            assertThat(pager.size(), equalTo(2));
+        }
+    }
+
+    @Nested
+    class RangeTests {
+        @Test
+        void shouldReturnRowsInRange() {
+            final int[] rows = rowsInRandomOrder(0, 100);
+            pager.add(iterable(rows));
+            final int[] view = new int[35];
+            pager.rowsInRange(
+                    40,
+                    35,
+                    new IntConsumer() {
+                        int pos = 0;
+
+                        @Override
+                        public void accept(final int row) {
+                            view[pos++] = row;
+                        }
+                    });
+            for (int i = 0; i < view.length; i++) {
+                assertThat(pager.position(view[i]), equalTo(i + 40));
+            }
+        }
+
+        @Test
+        void shouldReturnNothingWhenStartIsPastLimit() {
+            pager.add(iterable(IntStream.range(30, 45).toArray()));
+            final IntConsumer consumer = mock(IntConsumer.class);
+            pager.rowsInRange(15, 1, consumer);
+            verify(consumer, never()).accept(anyInt());
+        }
+
+        @Test
+        void shouldReturnFewerItemsWhenLimitIsPastEnd() {
+            pager.add(iterable(IntStream.range(30, 45).toArray()));
+            final IntConsumer consumer = mock(IntConsumer.class);
+            pager.rowsInRange(0, 20, consumer);
+            verify(consumer, times(15)).accept(anyInt());
+        }
+    }
+
+    private int[] rowsInRandomOrder(final int start, final int end) {
+        final var rowList = new ArrayList<>(IntStream.range(start, end).boxed().toList());
+        Collections.shuffle(rowList, rnd);
+        return rowList.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    private void assertRowOrder(final int... rows) {
+        for (int pos = 0; pos < rows.length; pos++) {
+            final int row = rows[pos];
+            assertThat(pager.position(row), equalTo(pos));
+            assertThat(pager.row(pos), equalTo(row));
+        }
+    }
+
+    private static IntIterable iterable(final int... values) {
+        return intConsumer -> {
+            for (final int value : values) {
+                intConsumer.accept(value);
+            }
+        };
+    }
+}
