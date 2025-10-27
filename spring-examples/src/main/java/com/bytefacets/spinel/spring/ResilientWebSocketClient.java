4f8422fc54772f49f98e9eabffed8a809e23d7cc
paulfeuer <code@bytefacets.com>
2025-10-27 12:05:17 -0400

    spotless

--- spring-examples/src/main/java/com/bytefacets/spinel/spring/ResilientWebSocketClient.java
+++ spring-examples/src/main/java/com/bytefacets/spinel/spring/ResilientWebSocketClient.java
@@ -22,8 +22,8 @@ import reactor.util.retry.Retry;
 import reactor.util.retry.RetryBackoffSpec;
 
 /**
- * This is an example of a WebSocketClient that is pointing at the spring boot server
- * in this package.
+ * This is an example of a WebSocketClient that is pointing at the spring boot server in this
+ * package.
  */
 public final class ResilientWebSocketClient {
     private static final Logger log = LoggerFactory.getLogger(ResilientWebSocketClient.class);
